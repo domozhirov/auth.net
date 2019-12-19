6a7878b5d66c51b468a8522b898386645067f45f
Domozhirov Mikhail <michael@domozhirov.ru>
2019-12-19 15:59:07 +0300

    dev

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 (function($) {
-
-
   $(() => {
     const $popup = $('.purchase-popup-wrapper');
-    const $popupHeader = $popup.find('.card-header');
-    const $popupTitle = $popup.find('.card-title');
+    const $popupHeader = $popup.find('.popup-card-header');
+    const $popupTitle = $popup.find('.popup-card-title');
     const popupHeaderMessage = $popupTitle.data('message');
-    const $popupClose = $popup.find('.card-header');
+    const $popupClose = $popup.find('.popup-card-header');
 
     $(document).on('click', '.purchase-btn', function() {
       const $this = $(this);
@@ -27,6 +25,12 @@
       event.preventDefault();
 
       $popup.hide();
-    })
+    });
+
+    const $popupAlert = $('.popup-alert');
+
+    $(document).on('click', '.popup-alert-header button, .popup-alert-footer button', function() {
+      $popupAlert.hide();
+    });
   });
 })(jQuery);
